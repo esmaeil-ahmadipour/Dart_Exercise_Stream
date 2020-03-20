6dfd906d01049956401ddfc1c6dd593568d3365f
Esmaeil Ahmadipour <software8686@gmail.com>
2020-03-20 17:53:43 +0330

    commit> State > ex1 > Handel Events (Basic)

--- 03_State/ex1/main.dart
+++ 03_State/ex1/main.dart
@@ -20,5 +20,46 @@ class StateMachine {
   State getState() {
     return _state;
   }
+
+  void On(Event event) {
+    var old = _state;
+
+    switch (event) {
+
+      case Event.START:
+        {
+            _state = State.STARTED;
+            print("Event.START / Application is Starting");
+        }
+        break;
+
+      case Event.GO:
+        {
+          _state = State.RUNNING;
+          print("Event.GO / Application is Go To Running");
+
+        }
+        break;
+
+      default:
+        {
+          print("Unhandeled Event: $event");
+        }
+    }
+    print("State {from:$old , to:$_state}");
+
+  }
 }
 
+void main() {
+  var stateMachine = StateMachine();
+
+  stateMachine.On(Event.START);
+  print("State Is: ${stateMachine.getState()} \n");
+
+  stateMachine.On(Event.GO);
+  print("State Is: ${stateMachine.getState()}\n");
+
+  stateMachine.On(Event.END);
+  print("State Is: ${stateMachine.getState()}\n");
+}
